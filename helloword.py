1f97976ae48c702d731f0f3e3048d4a40a736abd
zevolpato <zevolpato@outlook.com>
2023-11-30 15:17:19 -0300

    Create helloword.py
    first

---
+++ helloword.py
@@ -0,0 +1,7 @@
+print("Hello Word!") # Mostra na tela o texto Hello Word!
+mensagem = "Ola, tudo bem?"
+print(mensagem) # mostra a mensagem na tela
+print(mensagem[1]) #mostra a segunda letra da mensagem
+print(mensagem[0:4]) # mostra da primeira ate a 4 letra da mensagem
+print(mensagem[-1]) # mostra a ultima letra da mensagem
+print(mensagem[-4:-1])  # mostra da quarta posição até o fim 
