1025f6102149d1e308c3e7e3f53410e4e4d72a29
zevolpato <zevolpato@outlook.com>
2023-11-30 15:33:38 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+nome = "Jose"
+numero = 6
+troco = 4.65
+is_login = True # sempre com a primeira letra maiuscula True
+print(type(nome))
+print(type(numero))
+print(type(troco))
+print(type(is_login))
