cc33366bae7d994c92c3378b0cf4be2d06bccc1f
zevolpato <zevolpato@outlook.com>
2023-11-30 17:17:44 -0300

    Create calculadoraSimples.py

---
+++ calculadoraSimples.py
@@ -0,0 +1,7 @@
+#n1 = input("Digite o primeiro número : ")
+#n2 = input("Digite o segundo número : ")
+#print(int(n1)+int(n2))
+
+n1 = int(input("Digite o primeiro número : "))
+n2 = int(input("Digite o segundo número : "))
+print(n1+n2)
