aafcb620dcbdb3f8953cd2d30074dd518143d568
zevolpato <zevolpato@outlook.com>
2023-11-30 16:53:29 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -1,3 +1,4 @@
+"""
 nome = "Jose"
 numero = 6
 troco = 4.65
@@ -6,3 +7,18 @@ print(type(nome))
 print(type(numero))
 print(type(troco))
 print(type(is_login))
+"""
+nomes = [ "Maria", "João", "Fábio", "Ana", "Zé" ]
+print(type(nomes)) #mostrar o tipo List
+print(nomes) #mostra a lista
+# print(nomes[1]) # mostra a posição 2
+print(nomes.pop()) # exclui a ultima posição e mostra o nome excluido
+print(nomes) # mostrar a lista novamente
+nomes.pop() # exclui a ultima posicao
+print(nomes)
+nomes.append("Ana") #adiciona um nome no final da lista
+nomes.append("Lorenzo") 
+print(nomes)
+
+for x in nomes:
+    print(x)
